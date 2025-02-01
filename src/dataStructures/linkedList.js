c52532a19574b905bf8a63c9fb8c5708ed89b947
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-02-01 21:58:26 +0200

    feat: linked list styles
    Took 16 minutes

--- src/dataStructures/linkedList.js
+++ src/dataStructures/linkedList.js
@@ -17,10 +17,31 @@ export function render() {
     return `
     <div class="visualization linked-list">
       <div class="node-container">
-        <div class="node">Head</div>
-        <div class="node">Node 1</div>
-        <div class="node">Node 2</div>
-        <div class="node">Tail</div>
+        <div class="node">
+            <p class="square right-border">1</p>
+            <p class="square">2</p>
+            <span class="material-symbols-outlined">
+                arrow_right_alt
+            </span>
+        </div>
+        <div class="node">
+            <p class="square right-border">2</p>
+            <p class="square">7</p>
+            <span class="material-symbols-outlined">
+                arrow_right_alt
+            </span>
+        </div>
+        <div class="node">
+            <p class="square right-border">7</p>
+            <p class="square">15</p>
+            <span class="material-symbols-outlined">
+                arrow_right_alt
+            </span>
+        </div>
+        <div class="node">
+            <p class="square right-border">15</p>
+            <p class="square">null</p>
+        </div>
       </div>
     </div>
   `;
