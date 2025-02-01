a47f94f1d0465b6c435c8bf8def1bdfdd9f3d641
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-02-01 17:15:04 +0200

    feat: arrow to every item in list
    Took 15 minutes

--- src/components/navigationRender.js
+++ src/components/navigationRender.js
@@ -5,16 +5,37 @@ export function navigationRender(algorithms, dataStructures) {
     function createSection(titleText, items) {
         const section = document.createElement("section");
 
+        const titleContainer = document.createElement("div");
+        titleContainer.classList.add("flex");
         const title = document.createElement("h3");
         title.textContent = titleText;
-        section.appendChild(title);
+
+        const arrowItem = document.createElement("span");
+        arrowItem.className = 'material-symbols-outlined';
+        arrowItem.textContent = "keyboard_arrow_down";
+        arrowItem.setAttribute("data-view", titleText);
+
+        titleContainer.appendChild(title);
+        titleContainer.appendChild(arrowItem);
+
+        section.appendChild(titleContainer);
 
         const listEl = document.createElement("ul");
         items.forEach(itemData => {
+            const listContainer = document.createElement("div");
+            listContainer.classList.add("flex");
             const listItem = document.createElement("li");
             listItem.setAttribute("data-view", itemData.name);
             listItem.textContent = itemData.name;
-            listEl.appendChild(listItem);
+
+            const arrowItem = document.createElement("span");
+            arrowItem.className = 'material-symbols-outlined';
+            arrowItem.textContent = "keyboard_arrow_down";
+            arrowItem.setAttribute("data-view", itemData.name);
+
+            listContainer.appendChild(listItem);
+            listContainer.appendChild(arrowItem);
+            listEl.appendChild(listContainer);
         });
         section.appendChild(listEl);
         return section;
--- src/data/dataStructures.js
+++ src/data/dataStructures.js
@@ -2,6 +2,7 @@ const dataStructuresPath = '../dataStructures/';
 
 const dataStructures = [
     { name: 'stack', module: `${dataStructuresPath}stack.js` },
+    { name: 'linkedList', module: `${dataStructuresPath}linkedList.js` },
     { name: 'queue', module: `${dataStructuresPath}queue.js` },
 ];
 
