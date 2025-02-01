22812de5fa913b6ec7b3773bcc5ecdb41af0a153
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-02-01 22:20:41 +0200

    feat: make linked list dinamiclly render in html
    Took 13 minutes

--- src/dataStructures/linkedList.js
+++ src/dataStructures/linkedList.js
@@ -36,36 +36,22 @@ class LinkedList {
     }
 }
 
-export function render() {
-    return `
-    <div class="visualization linked-list">
-      <div class="node-container">
-        <div class="node">
-            <p class="square right-border">1</p>
-            <p class="square">2</p>
-            <span class="material-symbols-outlined">
-                arrow_right_alt
-            </span>
-        </div>
-        <div class="node">
-            <p class="square right-border">2</p>
-            <p class="square">7</p>
-            <span class="material-symbols-outlined">
-                arrow_right_alt
-            </span>
-        </div>
-        <div class="node">
-            <p class="square right-border">7</p>
-            <p class="square">15</p>
-            <span class="material-symbols-outlined">
-                arrow_right_alt
-            </span>
-        </div>
-        <div class="node">
-            <p class="square right-border">15</p>
-            <p class="square">null</p>
-        </div>
-      </div>
-    </div>
-  `;
+export function render(data) {
+    if(!data) return "<p>The data is empty</p>";
+
+    let current = data.head;
+    let html = `<div class="visualization linked-list"><div class="node-container">`;
+
+    while(current) {
+        html += `
+            <div class="node">
+                <p class="square right-border">${current.data}</p>
+                <p class="square">${current.next}</p>
+                ${current.next ? `<span class="material-symbols-outlined">arrow_right_alt</span>` : ""}
+            </div>`;
+        current = current.next;
+    }
+
+    html += '<div></div>'
+    return html;
 }
