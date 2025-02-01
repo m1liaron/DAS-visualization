b5af8bf23ef2583d82dd77ec5f5ac4f51c4cee86
Vlad <114946628+m1liaron@users.noreply.github.com>
2025-02-01 22:03:34 +0200

    feat: linked list methods
    Took 5 minutes

--- src/dataStructures/linkedList.js
+++ src/dataStructures/linkedList.js
@@ -11,6 +11,29 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    append(data) {
+        const newNode = new Node(data);
+        if(!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head
+            while(current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    toArray() {
+        const result = [];
+        let current = this.head;
+        while(current) {
+            result.push(current.data);
+            current = current.next;
+        }
+        return result;
+    }
 }
 
 export function render() {
